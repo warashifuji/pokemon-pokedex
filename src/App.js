9a4feb00f9ba26e6a2aaa105055794688b656a62
warashifuji <shuhei.22wara@gmail.com>
2024-01-10 17:53:25 +0900

    APIから呼び出し、最初の20匹を表示

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PokemonThumbnails from './PokemonThumbnails';
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [pokemonNames, setPokemonNames] = useState([]);
+  const pokemons = [
+    {
+      id: 1,
+      name: "フシギダネ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      type: "くさ"
+    },
+    {
+      id: 2,
+      name: "フシギソウ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      type: "くさ"
+    },
+    {
+      id: 3,
+      name: "フシギバナ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+      type: "くさ"
+    },
+  ]
+
+  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+  const createPokemonObject = () => {
+    fetch(pokemonUrl)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.sprites.other["official-artwork"].front_defaut);
+        console.log(data.types[0].type.name);
+        console.log(data.types[1].type.name);
+
+      })
+  }
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
+  const getAllPokemons = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results);
+        setAllPokemons(data.results);
+        setUrl(data.next);
+      });
+    };
+
+  useEffect(() => {
+    getAllPokemons();
+    createPokemonObject();
+  }, []);
+
+
   return (
-    <div className="App">
+    <div className="app-container">
+      <h1>ポケモン図鑑</h1>
+      <div className='pokemon-container'>
+        <div className='all-container'>
+          {pokemons.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              name={pokemonNames[index]}
+              image={pokemon.image}
+              type={pokemon.type} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/PokemonThumbnails.jsx
+++ src/PokemonThumbnails.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const PokemonThumnails = () => {
-    return <div>PokemonThumanails</div>;
-}
+const PokemonThumbnails = ({ id, name, image, type }) => {
+    return (
+        <div className="thumb-container grass">
+            <div className="number">
+                <small>#0{id}</small>
+            </div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h4>{name}</h4>
+                <h3>{type}</h3>
+            </div>
+        </div>
+    );
+};
 
-export default PokemonThumnails;
+export default PokemonThumbnails;
